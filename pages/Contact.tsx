b5b2ea3df16f94717c9c1054f0fe587afc9c3f2a
Josh Segatt <josh@vibestudio.dev>
2026-01-15 22:24:05 +0000

    Update: 3D WhatsApp icon for contact card

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -110,7 +110,7 @@ export const Contact: React.FC = () => {
             >
               <div className="w-24 h-24 mb-2 flex items-center justify-center transition-transform duration-300 group-hover:scale-110 drop-shadow-xl">
                 <img
-                  src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Mobile%20Phone.png"
+                  src="/whatsapp-3d.png"
                   alt="WhatsApp"
                   className="w-full h-full object-contain"
                 />
